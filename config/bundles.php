bf36a0f2669541c1f75fc33ec6742b2b2a59f30c
Abelkanga <kangaabel78@gmail.com>
2025-02-10 16:08:40 +0000

    remove mercure bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,4 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Flasher\Symfony\FlasherSymfonyBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
