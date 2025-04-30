b6a56cc926a34d92c9e3646e0a114f5a4fa22486
mossounAndre <andre.otron@gmail.com>
2025-04-30 13:12:38 +0000

    Add new menu

--- src/Form/ExpenseType.php
+++ src/Form/ExpenseType.php
@@ -6,6 +6,7 @@ use App\Entity\Expense;
 use App\Entity\TypeExpense;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +15,18 @@ class ExpenseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code')
-            ->add('name')
-            ->add('status')
+            ->add('code', TextType::class, [
+                'label' => 'N° de compte'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Intitulé'
+            ])
             ->add('autre')
             ->add('typeExpense', EntityType::class, [
                 'class' => TypeExpense::class,
-                'choice_label' => 'id',
+                'choice_label' => function (TypeExpense $typeExpense) {
+                    return sprintf('%s  %s', $typeExpense->getCode(), $typeExpense->getName());
+                },
             ])
         ;
     }
--- src/Form/TypeExpenseType.php
+++ src/Form/TypeExpenseType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\TypeExpense;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,12 @@ class TypeExpenseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code')
-            ->add('name')
-            ->add('status')
+            ->add('code', TextType::class, [
+                'label' => 'N° de compte'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Intitulé'
+            ])
         ;
     }
 
